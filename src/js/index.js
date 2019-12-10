f3e60a6ecda920b1acc7ac4b08330cd3eb0d20a2
gabsrodriguez <gmederos95@gmail.com>
2019-12-10 22:15:33 +0000

    inputs

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,3 @@ import "../assets/img/rigo-baby.jpg";
 import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
-
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
-};
